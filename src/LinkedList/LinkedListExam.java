ac67b590a1fa4ee656a7fb459741309c1dd72837
robot <robot@10.85.2.105>
2023-08-18 14:17:27 +0900

    LinkedList 수업준비

---
+++ src/LinkedList/LinkedListExam.java
@@ -0,0 +1,22 @@
+package LinkedList;
+
+import java.util.LinkedList;
+
+public class LinkedListExam {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		LinkedList<String> list= new LinkedList<>();
+		
+		list.add("유재석");
+		list.add("지석진");
+		list.add("김종국");
+		list.add("송지효");
+		list.add("하하");
+		
+		System.out.println(list);
+
+	}
+
+}
